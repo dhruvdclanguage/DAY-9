2778e9de13ea9b8eea0eea0fc2b807c6bc090908
dhruvdclanguage <126230133+dhruvdclanguage@users.noreply.github.com>
2023-02-28 12:38:33 +0530

    Add files via upload

---
+++ day 9.1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main (){
+
+	int a , b ;
+	
+	printf("Enter value A :");
+	scanf("%d",&a);
+	
+	printf("Enter value B :");
+	scanf("%d",&b);
+	
+	a>b
+	    ?
+	    
+	    printf("A is MAX")
+	    
+	    :
+	    	printf("B is MAX");
+
+	return 0 ;
+}
